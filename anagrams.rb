c6bb83f4bd31ed54d6017ee4c3043a4abffd6dc4
Ethanjonm <ethanjonmercado@gmail.com>
2023-03-30 14:51:56 -0400

    finish #second_anagram?

--- anagrams.rb
+++ anagrams.rb
@@ -6,3 +6,21 @@ def first_anagram?(str1,str2)
     str1_p.include?(str2_a)
 
 end
+# O(n^2)
+def second_anagram?(str1, str2)
+    str1_a = str1.split("")
+    str2_a = str2.split("")
+
+    str1_a.each do |char|
+        idx_del = str2_a.find_index(char)
+        if idx_del != nil
+        str2_a.delete_at(idx_del)
+        end
+    end
+    if str2_a.length == 0
+        return true
+    end
+    false
+
+end
+
